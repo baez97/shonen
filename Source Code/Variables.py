78edefba09b530e56a5cd804054a3715c75a5f25
Jose Manuel Baez <baezsoriano97@gmail.com>
2018-05-10 14:20:34 +0200

    Changing window size

--- Source Code/Variables.py
+++ Source Code/Variables.py
@@ -6,6 +6,6 @@ fpsClock       = pygame.time.Clock()
 
 WHITE          = (255, 255, 255)
 
-DISPLAY_WIDTH  = 400
-DISPLAY_HEIGHT = 300
+DISPLAY_WIDTH  = 600
+DISPLAY_HEIGHT = 400
 DISPLAYSURF    = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
