3be4a67db4bac0ee875040422982312f934ccde7
Jose Manuel Baez <baezsoriano97@gmail.com>
2018-05-16 19:39:13 +0200

    Adding Maze class

--- Source Code/Maze.py
+++ Source Code/Maze.py
@@ -3,16 +3,6 @@ from Variables import *
 class Maze:
     def __init__():
         self.celdas = [[]]
-        
-        DISPLAYSURF.fill(WHITE)
-
-        for x in range(0, 600, 50):
-            DISPLAYSURF.blit(grass, (x, 0))
-        for x in range(0, 560, 50):
-            for y in range(40, 321, 40):
-                DISPLAYSURF.blit(block, (x,y))
-                
-        DISPLAYSURF.blit(grass, (0, 20))
 
 class Tile:
     def __init__():
