b50d4c19d7847e679beb77e40fbeb16aee15e7f1
Jose Manuel Baez <baezsoriano97@gmail.com>
2018-05-16 17:57:10 +0200

    Adding Maze class

---
+++ Source Code/Maze.py
@@ -0,0 +1,11 @@
+from Grafico import *
+class Tile:
+    def __init__():
+        self.grafico = TileGrafico(self)
+    def pintar():
+        self.grafico.pintar()
+
+class GrassTile(Tile):
+    def __init__():
+        self.imagen = 'images/grass.png'
+        super(GrassTile, self).__init__()
